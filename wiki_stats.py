24740794db6bfcdd448494fdf368a150e0bab250
chelberserker <bersenjow@gmail.com>
2016-03-21 13:06:03 +0400

    update

--- wiki_stats.py
+++ wiki_stats.py
@@ -20,38 +20,63 @@ class WikiGraph:
         print('Загружаю граф из файла: ' + filename)
 
         with open(filename) as f:
-            (n, _nlinks) = (0, 0) # TODO: прочитать из файла
+
+            (n, _nlinks) = (map(int, f.readline().split()))
             
             self._titles = []
             self._sizes = array.array('L', [0]*n)
             self._links = array.array('L', [0]*_nlinks)
             self._redirect = array.array('B', [0]*n)
             self._offset = array.array('L', [0]*(n+1))
-
-            # TODO: прочитать граф из файла
-
+            n_lks = 0 # current number of links
+            for i in range(n):
+                self._titles.append(f.readline())
+                (size, redirect, lks) = (map(int, f.readline().split()))
+                self._sizes[i] = size
+                self._redirect[i] = redirect
+                for j in range(n_lks, n_lks + lks):
+                    self._links[j] = int(str(f.readline()))
+                n_lks += lks
+                if n > 0:
+                    self._offset[i+1] = self._offset[i] + lks
         print('Граф загружен')
 
     def get_number_of_links_from(self, _id):
-        pass
+        return len(self._links[self._offset[_id]:self._offset[_id+1]])
 
     def get_links_from(self, _id):
-        pass
+        return self._links[self._offset[_id]:self._offset[_id+1]]
 
     def get_id(self, title):
-        pass
+        for i in range(len(self._titles)):
+            if self._titles[i] == title:
+                return(i)
 
     def get_number_of_pages(self):
-        pass
+        return len(self._titles)
 
     def is_redirect(self, _id):
-        pass
+        return self._redirect[_id]
 
     def get_title(self, _id):
-        pass
+        return self._titles[_id]
 
     def get_page_size(self, _id):
-        pass
+        return self._sizes(_id)
+
+def analyse_links_from_page(G):
+    numlinks_from = list(map(G.get_number_of_links_from, range(G.get_number_of_pages())))
+    _max = max(numlinks_from)
+    _min = min(numlinks_from)
+    mxn = sum(x == _max for x in numlinks_from)
+    mnn = sum(x == _min for x in numlinks_from)
+    print("Minimal number of links from page:", _min)
+    print("Pages with minimal number of links:", mnn)
+    print("Maximal number of links from page:", _max)
+    print("Pages with maximal number of links:", mxn)
+    print("Mean number of links: %0.2f  (st. dev. : %0.2f)" %(statistics.mean(numlinks_from), statistics.stdev(numlinks_from)))
+
+
 
 
 def hist(fname, data, bins, xlabel, ylabel, title, facecolor='green', alpha=0.5, transparent=True, **kwargs):
@@ -71,5 +96,10 @@ if __name__ == '__main__':
     else:
         print('Файл с графом не найден')
         sys.exit(-1)
+
+    print("Number of pages with redirect:", sum(wg._redirect))
+    analyse_links_from_page(wg)
+
+
 
     # TODO: статистика и гистограммы
